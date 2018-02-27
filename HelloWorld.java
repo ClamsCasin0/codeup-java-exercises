c91d537d5395fcd7960577779284802308b07ccc
armandosegura <segura_arm@yahoo.com>
2018-02-27 10:58:31 -0600

    java push

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("suck a lotta eggs");
+        System.out.print("That's what my daddy used to say");
+        System.out.print("but now he's gone away");
+    }
+}
