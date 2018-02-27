046e084cf880bfbb7753494cdaddb3d39a6bf980
armandosegura <segura_arm@yahoo.com>
2018-02-27 13:22:44 -0600

    variables java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,38 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.print("suck a lotta eggs");
+        System.out.println("suck a lotta eggs");
         System.out.print("That's what my daddy used to say");
         System.out.print("but now he's gone away");
+
+
+        int favoriteNumber = 17;
+        System.out.println(favoriteNumber);
+
+        String myString = "An apple a day, keeps the playas at bay";
+        System.out.println(myString);
+
+        float myNumber = 123;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        int x = 4;
+        x += 5;
+        System.out.println(x);
+
+        int z = 3;
+        int t = 4;
+        t *= z;
+        System.out.println(t);
+
+        int u = 10;
+        int v = 2;
+        u /= v;
+        System.out.println(u);
+        v -= u;
+        System.out.println(v);
     }
 }
