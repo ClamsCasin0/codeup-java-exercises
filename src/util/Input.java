dada0dc4b4c511c4fdd8804885363d9ae3a85fa1
armandosegura <segura_arm@yahoo.com>
2018-03-02 15:38:08 -0600

    bonus

--- src/util/Input.java
+++ src/util/Input.java
@@ -10,10 +10,26 @@ public class Input {
             return this.sc.next();
         }
 
+        public String getString(String prompt) {
+            System.out.println(prompt);
+            return this.sc.next();
+        }
 
         public boolean yesNo() {
-            String input = this.sc.next();
-            if(input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")) {
+            String input1 = this.sc.next();
+            if(input1.equalsIgnoreCase("y") || input1.equalsIgnoreCase("yes")) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public boolean yesNo(String prompt) {
+            System.out.println(prompt);
+
+            String input2 = this.sc.next();
+
+            if(input2.equalsIgnoreCase("y") || input2.equalsIgnoreCase("yes")) {
                 return true;
             } else {
                 return false;
@@ -21,33 +37,70 @@ public class Input {
         }
 
 
-
         public int getInt(int min, int max) {
-            System.out.println("Please input an integer between " + min + " and " + max: );
-            int userInput = this.getInt();
+            System.out.println("Please input an integer between " + min + " and " + max);
+            int userInput3 = this.getInt();
 
-            if(userInput > max || userInput < min) {
+            if(userInput3 > max || userInput3 < min) {
                 return getInt(min, max);
             } else {
-                return userInput;
+                return userInput3;
             }
         }
 
+        public int getInt(int min, int max, String prompt) {
+            System.out.println(prompt);
+
+            int userInput4 = this.getInt();
+
+            if(userInput4 > max || userInput4 < min) {
+                return getInt(min, max);
+            } else {
+                return userInput4;
+            }
+        }
+
+
         public int getInt(){
+            return this.sc.nextInt();
+        }
+
+        public int getInt(String prompt) {
+            System.out.println(prompt);
             return this.sc.nextInt();
         }
 
 
         public double getDouble(double min, double max) {
             System.out.println("Please input a double integer bewtween " + min + " and " + max);
-            double userinput3 = this.getDouble();
+            double userinput5 = this.getDouble(min, max);
 
-            if(userinput3 > max || userinput3 < min) {
+            if(userinput5 > max || userinput5 < min) {
                 return getDouble(min, max);
             } else {
-                return userinput3;
+                return userinput5;
             }
         }
 
+        public double getDouble(double min, double max, String prompt) {
+            System.out.println(prompt);
+
+            double userInput6 = this.getDouble();
+
+            if(userInput6 > max || userInput6 < min) {
+                return getDouble(min, max);
+            } else {
+                return userInput6;
+            }
+        }
+
+        public double getDouble() {
+            return this.sc.nextDouble();
+        }
+
+        public double getDouble(String prompt) {
+            System.out.println(prompt);
+            return this.sc.nextDouble();
+        }
 
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -13,6 +13,25 @@ public class InputTest {
         int betweenInt = inPut.getInt(1, 100);
         System.out.println(betweenInt);
 
+
+        System.out.println("Please input a double Integer");
+        double dblDbl = inPut.getDouble();
+        System.out.println(dblDbl);
+
+        double anotherDouble = inPut.getDouble(1.0, 20.0);
+        System.out.println(anotherDouble);
+
+        System.out.println("Please input yes or a no");
+
+        if(inPut.yesNo()) {
+            System.out.println("You input yes or 'y'");
+        } else {
+            System.out.println("You said anything other than yes.");
+        }
+
+        String someString = inPut.getString();
+        System.out.println("You provided: " + someString);
+
     }
 
 }
