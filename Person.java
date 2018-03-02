7283805894bc9d7be51c6ce5ba45109f31ef5300
armandosegura <segura_arm@yahoo.com>
2018-03-02 13:42:13 -0600

    after lunch commit, working on curriculum exercise

--- Person.java
+++ Person.java
@@ -1,2 +1,54 @@
+
+
+
 public class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Howdy there, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+
+        Person per = new Person("Montel");
+
+        per.setName("Lafawndah");
+
+        System.out.println(per.getName());
+
+        per.sayHello();
+
+//       test1
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+        //test2
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        //test3
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+    }
 }
