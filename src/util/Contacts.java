062a2cb64492a1bb301b2f1f1ee0616200ccd7fd
armandosegura <segura_arm@yahoo.com>
2018-03-12 11:06:05 -0500

    monday morning grind

---
+++ src/util/Contacts.java
@@ -0,0 +1,29 @@
+package util;
+
+public class Contacts {
+
+    private String name;
+
+    private double phoneNumber;
+
+    public Contacts(String name, double phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(double phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
